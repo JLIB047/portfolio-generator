f2194bd3ec1a8dbede47683890f77643adf2d907
Jason Libertelli <jason.libertelli@gmail.com>
2021-11-17 22:53:05 -0500

    map() and filter()

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inquirer = require("inquirer");
+const fs = require('fs');
+const generatePage = require(`./src/page-template`);
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -136,15 +138,13 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+
+      fs.writeFile('./index.html', pageHTML, err => {
+      if(err) throw err;
+      /*
+      console.log('Portfolio complete! Check out index.html to see the output!');
+      });*/
   });
-/*const fs = require('fs');
-const generatePage = require(`./src/page-template`);
-const pageHTML = generatePage(user, github);
-
-
-fs.writeFile('./index.html', pageHTML, err => {
-    if(err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!');
-}); */
+})
